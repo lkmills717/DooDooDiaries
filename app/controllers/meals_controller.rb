12a2a83926b499bbe84880c6ff7f24547cf6a6c2
Lara Mills <lkm347@gmail.com>
2020-08-31 17:51:40 +0100

    style fix

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,50 +1,49 @@
 class MealsController < ApplicationController
-        before_action :set_meal, only: [:show, :edit, :update, :destroy]
+    before_action :set_meal, only: [:show, :edit, :update, :destroy]
     
-        def index
-            @meal = Meal.all
-        end
-    
-        def show
-        end
-    
-        def new
-            @meal = Meal.new
-        end
-    
-        def create
-            @meal = Meal.new(meal_params)
-            if @meal.save
-                redirect_to meal_path(@meal)
-            else
-                render 'new'
-            end
-        end
-    
-        def edit
-        end
-    
-        def update
-            @meal = Meal.update(meal_params)
-            if @meal.save
-                redirect_to meal_path(@meal)
-            else
-                render 'new'
-            end
-        end
-    
-        def destroy
-            @meal.destroy
-            redirect_to new_meal_path
-        end
-    
-        private
-        def meal_params
-            params.require(:meal).permit(:id, :description)
-        end
-    
-        def set_meal
-            @meal = Meal.find(params[:id])
-        end
+    def index
+        @meal = Meal.all
+    end
 
+    def show
+    end
+
+    def new
+        @meal = Meal.new
+    end
+
+    def create
+        @meal = Meal.new(meal_params)
+        if @meal.save
+            redirect_to meal_path(@meal)
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @meal = Meal.update(meal_params)
+        if @meal.save
+            redirect_to meal_path(@meal)
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        @meal.destroy
+        redirect_to new_meal_path
+    end
+
+    private
+    def meal_params
+        params.require(:meal).permit(:id, :description)
+    end
+
+    def set_meal
+        @meal = Meal.find(params[:id])
+    end
 end
