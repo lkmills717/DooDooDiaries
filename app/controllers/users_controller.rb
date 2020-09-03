dce4a9dcd1bc0db647dffc093f17ce5a89a4508c
Gabriel Neves <gabriel.bneves@gmail.com>
2020-09-03 10:55:57 +0100

    fixing profile page bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def profile
     @movement = current_user.movements.last
-    @meal = current_user.meals.last
+    # @meal = current_user.meals.last
   end
 
 end
