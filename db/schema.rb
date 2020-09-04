b925e8f45a98e11a5a8a26854c6b6d384ee0743b
Richard Barrett <richardbarrett1981@hotmail.com>
2020-09-04 15:08:46 +0100

    image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_133812) do
+ActiveRecord::Schema.define(version: 2020_09_04_140224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
