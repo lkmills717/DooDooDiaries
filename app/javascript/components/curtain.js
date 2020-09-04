6a3f3f3f6e43e75f9a0ca38b72dbc41eeca4e6a2
Gabriel Neves <gabriel.bneves@gmail.com>
2020-09-04 10:02:08 +0100

    landing page effects

--- app/javascript/components/curtain.js
+++ app/javascript/components/curtain.js
@@ -1,9 +1,11 @@
 const curtain = () => {
-  const test = document.querySelector('.invitation');
+  const test = document.querySelector('.checkbox');
   
   test.addEventListener('click', () => {
     document.querySelector('.home-btns').className += '--on';
     document.querySelector('.invitation').className += '__off';
+    document.querySelector('.checkbox').className += '__off';
+    // document.querySelector('.curtain__panel').className += '--on';
   });
 }
 
