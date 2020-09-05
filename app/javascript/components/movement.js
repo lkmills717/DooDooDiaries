bf200ed1856a7203628416432a49e4788f9781d1
Gabriel Neves <gabriel.bneves@gmail.com>
2020-09-05 20:32:57 +0100

    javascript is working on new movement :)

--- app/javascript/components/movement.js
+++ app/javascript/components/movement.js
@@ -1,15 +1,18 @@
 const movement = () => {
   const fields = document.querySelectorAll('.btn-shape');
+  console.log(fields);
 
 
   fields.forEach(field => {
     field.addEventListener('click', () => {
+      console.log(field);
       const data = field.getAttribute('data-id');
-      console.log(data[0]);
+      console.log(data);
       document.querySelector('#movement_shape').value = data;
-      // const img = field.src;
-      // console.log(img);
-      // document.querySelector('#shape_choice').src = img;
+      const img = field.src;
+      console.log(img);
+      document.querySelector('#shape_choice').className += '--on';
+      document.querySelector('#shape_choice').src = img;
     });
    });
 }
