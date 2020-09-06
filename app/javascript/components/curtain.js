18007cc6818f2d2c9144f31a125e1cf38064dc31
Gabriel Neves <gabriel.bneves@gmail.com>
2020-09-06 16:21:06 +0100

    replaced css checkbox with js

--- app/javascript/components/curtain.js
+++ app/javascript/components/curtain.js
@@ -1,10 +1,13 @@
 const curtain = () => {
-  const test = document.querySelector('.checkbox');
+  const test = document.querySelector('.curtain');
   
   test.addEventListener('click', () => {
     document.querySelector('.home-btns').className += '--on';
+    document.querySelector('.curtain__panel--left').className += '--on';
+    document.querySelector('.curtain__panel--right').className += '--on';
     document.querySelector('.invitation').className += '__off';
-    document.querySelector('.checkbox').className += '__off';
+    document.querySelector('.invitation').className += '__off';
+    // document.querySelector('.checkbox').className += '__off';
     // document.querySelector('.curtain__panel').className += '--on';
   });
 }
