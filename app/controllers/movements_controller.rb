d9c3a7abfd0345014f1a6b577acc5b107bf70bff
Lara Mills <lkm347@gmail.com>
2020-09-07 16:44:21 +0100

    cant look at other poops

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -10,6 +10,9 @@ class MovementsController < ApplicationController
     end
 
     def show
+      unless current_user == @movement.user
+        redirect_to root_path, notice: 'stop looking at other people stool'
+      end
     end
 
     def new
