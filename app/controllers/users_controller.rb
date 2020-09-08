baeb4df6d509b6235d503ebf5ee11bc2fd711bec
Richard Barrett <richardbarrett1981@hotmail.com>
2020-09-08 15:47:37 +0100

    filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,22 @@ class UsersController < ApplicationController
     @meal = current_user.meals.last
     @movements = current_user.movements
     @meals = current_user.meals
-    if params[:query].present?
-     @movements = @movements.where("created_at > ?", params[:query])
-     @meals = @meals.where("created_at > ?", params[:query])
-     @timeline = @movements + @meals
-     @sorted = @timeline.sort_by &:created_at
+    if params[:start_date] && params[:end_date]
+      if params[:type] == "Movements"
+        @movements = @movements.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
+        @meals = []
+      elsif params[:type] == "Meals"
+        @meals = @meals.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
+      @movements = []
+      elsif  params[:type] == "Meals && Movements"
+        @movements = @movements.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
+        @meals = @meals.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
+      end
+      @timeline = @movements + @meals
+      @sorted = @timeline.sort_by &:created_at
     else
-     @timeline = @movements + @meals
-     @sorted = @timeline.sort_by &:created_at
+      @timeline = @movements + @meals
+      @sorted = @timeline.sort_by &:created_at
     end
   end
 
