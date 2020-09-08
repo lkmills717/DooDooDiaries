d6d32cd5de8b3837ac6f39e18bc2712bb73d09c3
Richard Barrett <richardbarrett1981@hotmail.com>
2020-09-08 13:46:35 +0100

    filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,15 @@ class UsersController < ApplicationController
     @meal = current_user.meals.last
     @movements = current_user.movements
     @meals = current_user.meals
-    @timeline = @movements + @meals
-    @sorted = @timeline.sort_by &:created_at
+    if params[:query].present?
+     @movements = @movements.where("created_at > ?", params[:query])
+     @meals = @meals.where("created_at > ?", params[:query])
+     @timeline = @movements + @meals
+     @sorted = @timeline.sort_by &:created_at
+    else
+     @timeline = @movements + @meals
+     @sorted = @timeline.sort_by &:created_at
+    end
   end
 
 end
