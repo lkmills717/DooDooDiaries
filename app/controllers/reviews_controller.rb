32c32d9fa9468878f2519b3f0a62824a961fddac
Lara Mills <lkm347@gmail.com>
2020-09-09 15:42:41 +0100

    user can view where they pooped

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+    def new
+        @review = Review.new
+        @bathroom = Bathroom.find(params[:bathroom_id])
+    end
+
+    def create
+        @bathroom = Bathroom.find(params[:bathroom_id])
+        @user = current_user
+        @review = Review.new(review_params)
+        @review.bathroom = @bathroom
+        @review.user = @user
+        if @review.save
+            flash[:notice] ="🎉 Thank you for reviewing #{@bathroom.name}! 🚽"
+            redirect_to bathroom_path(@bathroom)
+          else
+            render :new
+        end
+    end
+
+    private
+    def review_params
+        params.require(:review).permit(:content, :rating)
+    end
 end
