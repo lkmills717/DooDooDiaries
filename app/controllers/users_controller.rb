4bb2fb77469f6e0e9124b69607bc08bb42b7c0f3
Lara Mills <lkm347@gmail.com>
2020-09-10 19:02:22 +0100

    poop in order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,23 +6,23 @@ class UsersController < ApplicationController
     @meal = current_user.meals.last
     @movements = current_user.movements
     @meals = current_user.meals
-    if params[:start_date] && params[:end_date]
-      if params[:type] == "Movements"
-        @movements = @movements.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
-        @meals = []
-      elsif params[:type] == "Meals"
-        @meals = @meals.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
-      @movements = []
-      else
-        @movements = @movements.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
-        @meals = @meals.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
-      end
+    # =begin if params[:start_date] && params[:end_date]
+    #   if params[:type] == "Movements"
+    #     @movements = @movements.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
+    #     @meals = []
+    #   elsif params[:type] == "Meals"
+    #     @meals = @meals.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
+    #     @movements = []
+    #   else
+    #     @movements = @movements.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
+    #     @meals = @meals.where("created_at > ? and created_at < ?",params[:start_date], params[:end_date])
+    #   end
+    #   @timeline = @movements + @meals
+    #   @sorted = @timeline.sort_by &:created_at
+    # else =end
       @timeline = @movements + @meals
       @sorted = @timeline.sort_by &:created_at
-    else
-      @timeline = @movements + @meals
-      @sorted = @timeline.sort_by &:created_at
-    end
+    # end
   end
 
   def update
